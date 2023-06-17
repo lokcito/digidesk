27f14753c8142585b4cb206f2b46c41ae8931ebc
lokcito <enciso.leo@gmail.com>
2023-06-17 06:12:41 +0000

    run

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,18 @@
-python manage.py collectstatic --noinput
+#!/bin/bash
 
+# Create a virtual environment
+echo "Creating a virtual environment..."
+python -m venv venv
+source venv/bin/activate
 
-#
+# Install the latest version of pip
+echo "Installing the latest version of pip..."
+python -m pip install --upgrade pip
+
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
